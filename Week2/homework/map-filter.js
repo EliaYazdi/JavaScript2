e205c2ce229693dd8076c64aa40ed55c932ab2b0
Elia <elahehahmadiehyazdi@gmail.com>
2019-08-02 17:10:10 +0200

    map.filter

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -3,14 +3,16 @@
 const myNumbers = [1, 2, 3, 4];
 
 const doubleOddNumbers = numbers => {
-  const odds = numbers.filter(function(number) {
+  const odds = numbers.filter(function (number) {
     return number % 2 !== 0;
   });
-  const doubles = odds.map(function(number) {
+  const doubles = odds.map(function (number) {
     return number * 2;
   });
   return doubles;
 };
+
+m
 
 console.log(doubleOddNumbers(myNumbers));
 
